32e616d4aae7a9cb4e6fc1386c1dec871c2c6601
alvaroRodri23 <alvrod951@alu.centroafuera.es>
2024-02-09 12:28:34 +0100

    last update

--- app/src/main/java/com/example/proyecto_si/Principal.kt
+++ app/src/main/java/com/example/proyecto_si/Principal.kt
@@ -1,11 +1,17 @@
 package com.example.proyecto_si
 
 import android.os.Bundle
+import com.google.firebase.Firebase
 
 
 class Principal {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.principal)
 
 
+
+    }
 
 
 
