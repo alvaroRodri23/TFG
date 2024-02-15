e6196d074eec9531e7eb4d925d449e3c0bea3e17
alvaroRodri23 <alvrod951@alu.centroafuera.es>
2024-02-15 12:32:38 +0100

    LAS version

--- app/src/main/java/com/example/proyecto_si/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_si/MainActivity.kt
@@ -6,32 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
-import com.google.firebase.Firebase
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.auth
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var auth: FirebaseAuth
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = Firebase.auth
-        // Evento personalizado para Google Analytics
-        val analytics : FirebaseAnalytics = FirebaseAnalytics.getInstance(this)
-        val bundle = Bundle()
-        bundle.putString("message","Integración de mensaje completa")
-        analytics.logEvent("InitScreen", bundle)
-
-        acceder()
 
     }
 }
