2424b7658eb61ff788333eaabee19e523342ca55
alvaroRodri23 <alvrod951@alu.centroafuera.es>
2024-02-16 13:24:20 +0100

    lastversion

--- app/src/main/java/com/example/proyecto_si/Principal.kt
+++ app/src/main/java/com/example/proyecto_si/Principal.kt
@@ -11,6 +11,7 @@ import android.widget.ImageView
 import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
 
+
 class Principal : AppCompatActivity() {
 
     private lateinit var seriesButton: ImageButton
@@ -68,6 +69,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Jujutsu Kaisen Season 2")
             intent.putExtra("imagen", R.drawable.jjks2)
             intent.putExtra("sinopsis", "A las 7:00 PM del 31 de octubre de 2018, aparece una extraña Pantalla cubriendo todo el Distrito de Shibuya, la cuál permite que los civiles y chamanes entren pero no puedan salir. Los habitantes encerrados dentro de la pantalla piden por la aparición de Satoru Gojo, ya que su aparición es la única condición que necesitan para salir.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=a70_eOnIS3o&ab_channel=CrunchyrollenEspa%C3%B1ol")
             startActivity(intent)
         }
 
@@ -76,6 +78,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Dr.Stone New World")
             intent.putExtra("imagen", R.drawable.drstone)
             intent.putExtra("sinopsis", "Tras el final de la \"Stone Wars\", los antiguos miembros del imperio de Tsukasa se unen al reino de la Ciencia para construir un barco capaz de surcar los mares y buscar respuestas al misterio global de la petrificación, pero no será sin problemas ya que necesitan nuevos avances para poner en funcionamiento el plan.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=bXgip0F6qdc&ab_channel=Crunchyroll")
             startActivity(intent)
         }
 
@@ -84,6 +87,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Ataque a los titanes")
             intent.putExtra("imagen", R.drawable.shingeki)
             intent.putExtra("sinopsis", "Un pequeño porcentaje de la humanidad consigue sobrevivir amurallando a sí mismos en una ciudad protegida por las murallas más altas que el más grande de los titanes. En el presente, la ciudad no ha visto a ningún titán en más de 100 años. Pero Eren y su hermana adoptiva Mikasa presencian algo horrible: las murallas de la ciudad son destruidas por un titán colosal que aparece de la nada. No pasará mucho tiempo hasta que los gigantes entran por el hueco abierto en el muro y comienzan a devorar a la gente.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=FRn6xXXF-7s&ab_channel=CrunchyrollenEspa%C3%B1ol")
             startActivity(intent)
         }
 
@@ -92,6 +96,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Dragon Ball Super")
             intent.putExtra("imagen", R.drawable.db)
             intent.putExtra("sinopsis", "Su trama describe las aventuras de Goku, un guerrero saiyajin experto en artes marciales, diez años después de la derrota de Majin Boo. En su travesía pone a prueba y mejora sus habilidades de pelea, enfrentando oponentes y protegiendo a la Tierra de los seres que quieren conquistarla y exterminar a la humanidad.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=yQJM963be_I&ab_channel=IGN")
             startActivity(intent)
         }
 
@@ -100,6 +105,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Black Clover")
             intent.putExtra("imagen", R.drawable.bc)
             intent.putExtra("sinopsis", "Black Clover sigue la historia de dos chicos, Asta y Yuno, que crecen en un orfanato de la iglesia de Hage, ubicada en el Reino del Trébol. Ambos se han criado en un mundo donde todas las personas tienen la habilidad de manipular el Poder Mágico.\n")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=TiIZ2NZxjMI&ab_channel=CrunchyrollenEspa%C3%B1ol")
             startActivity(intent)
         }
 
@@ -108,6 +114,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Black Clover Sword of the Wizard King")
             intent.putExtra("imagen", R.drawable.bcfilm)
             intent.putExtra("sinopsis", "Conrad Leto, el rey mago predecesor de Julius Novachrono, una vez respetado por la gente de Clover Kingdom pero de repente se rebeló contra el reino y fue sellado, ha resucitado. Ahora pretende usar la \"Espada Imperial\" para resucitar a los 3 Reyes Magos más temidos de la historia, Edward Avalaché, Princia Funnybunny y Jester Garandaros, y apoderarse del Reino Clover.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=PrgxJ1_sUcs&ab_channel=Netflix")
             startActivity(intent)
         }
 
@@ -116,6 +123,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Jujutsu Kaisen 0")
             intent.putExtra("imagen", R.drawable.jjk0)
             intent.putExtra("sinopsis", "Yuta Okkotsu es un chico de instituto bastante nervioso con un grave problema: Su amigo Rika se ha convertido en un Curse y no le deja a solas. Ya que Rika no es un Curse ordinario, capta la atención de Satoru Gojo, un profesor del Instituto Jujutsu, donde los exorcistas aprenden a combatir a los Curse. Gojo convence a Yuta para enrolarse en al academia... ¿Podrá aprender Yuta a controlar al Curse que le atormenta?.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=Yl_f0sYMaGk&ab_channel=Crunchyroll")
             startActivity(intent)
         }
 
@@ -125,6 +133,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Dragon Ball Super Broly")
             intent.putExtra("imagen", R.drawable.dbsbroly)
             intent.putExtra("sinopsis", "La paz ha regresado una vez más a la Tierra tras el Torneo de Poder. Al descubrir que en los diferentes universos hay seres increíblemente poderosos que aún no ha visto, Goku tiene intención de seguir entrenando para hacerse aún más fuerte. Entonces, un día, un saiyan llamado Broly al que nunca antes han visto se presenta ante Goku y Vegeta.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=mxUEUrj8Ezw&ab_channel=FandangoLatam")
             startActivity(intent)
         }
 
@@ -133,6 +142,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Suzume")
             intent.putExtra("imagen", R.drawable.suzume)
             intent.putExtra("sinopsis", "Suzume, de 17 años, descubre una misteriosa puerta en las montañas, y pronto empiezan a aparecer otras puertas por todo Japón. Cuando las puertas se abren, liberan desastres y destrucción, y depende de Suzume volver a cerrarlas.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=2rPPppw6Bhk&ab_channel=CrunchyrollenEspa%C3%B1ol")
             startActivity(intent)
         }
 
@@ -141,6 +151,7 @@ class Principal : AppCompatActivity() {
             intent.putExtra("titulo", "Guardianes de la noche")
             intent.putExtra("imagen", R.drawable.tren)
             intent.putExtra("sinopsis", "Tanjiro Kamado y sus amigos del Demon Slayer Corps acompañan a Kyōjurō Rengoku, el Flame Hashira, para investigar una misteriosa serie de desapariciones que ocurren dentro de un tren aparentemente infinitamente largo. Poco saben que Enmu, la última de las Lunas Inferiores de los Doce Kizuki, también está a bordo y les ha preparado una trampa.")
+            intent.putExtra("URL","https://www.youtube.com/watch?v=-AwLMRgcEoA&ab_channel=Cin%C3%A9polis")
             startActivity(intent)
         }
 
--- app/src/main/java/com/example/proyecto_si/infopelis.kt
+++ app/src/main/java/com/example/proyecto_si/infopelis.kt
@@ -1,12 +1,13 @@
 package com.example.proyecto_si
-
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.proyecto_si.R
 
 class infopelis : AppCompatActivity() {
 
@@ -15,24 +16,34 @@ class infopelis : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.infopelis)
 
-        findViewById<Button>(R.id.botonatrasinfo).setOnClickListener {
-            val intent = Intent(this, Principal::class.java)
-            startActivity(intent)
-        }
-
-
-
         // Obtener datos del Intent
         val titulo = intent.getStringExtra("titulo")
         val imagenResId = intent.getIntExtra("imagen", 0) // 0 es el valor predeterminado si no se encuentra la clave
         val sinopsis = intent.getStringExtra("sinopsis")
+        val URLvideo = intent.getStringExtra("URL")
 
         // Establecer datos en la UI
-
         findViewById<TextView>(R.id.Tituloinfo).text = titulo
         findViewById<ImageView>(R.id.fotoinfo).setImageResource(imagenResId)
         findViewById<TextView>(R.id.Sinopsis).text = sinopsis
 
-
+        // Configurar un OnClickListener para abrir el video en YouTube cuando se hace clic en el botón "trailer"
+        val trailerButton = findViewById<Button>(R.id.Trailer)
+        trailerButton.setOnClickListener {
+            // Intent para abrir la URL en la aplicación de YouTube
+            val youtubeIntent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + Uri.parse(URLvideo).getQueryParameter("v")))
+            // Si la aplicación de YouTube no está instalada, abrir la URL en el navegador web
+            if (youtubeIntent.resolveActivity(packageManager) != null) {
+                startActivity(youtubeIntent)
+            } else {
+                val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(URLvideo))
+                startActivity(webIntent)
+            }
+        }
+        val backButton = findViewById<Button>(R.id.botonatrasinfo)
+        backButton.setOnClickListener {
+            val intent = Intent(this, Principal::class.java)
+            startActivity(intent)
+        }
     }
 }
